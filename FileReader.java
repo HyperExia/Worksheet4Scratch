6ae3fb30b6bb834d6a3a01385fd9de8a2d206b3b
Brennan <chan.brennan@gmail.com>
2020-06-05 12:06:04 -0700

    w

---
+++ FileReader.java
@@ -0,0 +1,24 @@
+
+/**
+ * Abstract class FileReader - write a description of the class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+import java.io.File;
+public abstract class FileReader
+{
+    // instance variables - replace the example below with your own
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int findFileType(File file)
+    {
+        // put your code here
+        return 0;
+    }
+}
